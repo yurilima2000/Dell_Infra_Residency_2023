ac2b8a3dfad840833039f81826f08fb730173fcd
Yuri Lima Castilhos de Oliveira <yuri-oliveira@EGR.REDE.RS>
2024-02-29 12:07:00 -0300

    a

--- src/App.java
+++ src/App.java
@@ -1,5 +1,31 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+  
+    public void execute(){
+        readAndPrintCSV();
+    }
+
+
+
+    public void readAndPrintCSV() {        
+
+        try (BufferedReader reader = new BufferedReader(new FileReader("DNIT-Distancias.csv"))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                // Use StringTokenizer ou split() para dividir a linha em campos
+                String[] fields = line.split(",");
+                
+                // Imprima os campos
+                for (String field : fields) {
+                    System.out.print(field + " ");
+                }
+                System.out.println(); // Nova linha para a próxima linha do CSV
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Main{
+    public static void main(String[] args) {
+       App app = new App();
+       app.execute();
+    }
+
+
+}
