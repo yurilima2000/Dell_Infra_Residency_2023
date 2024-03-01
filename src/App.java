b4ebfca27c9ff90207ac180edc5e4e9e61bc9645
Yuri Lima <y.castilhos@edu.pucrs.br>
2024-02-29 23:17:01 -0300

    ˜dd˜

--- src/App.java
+++ src/App.java
@@ -10,16 +10,38 @@ import java.util.Scanner;
 public class App {
 
     private ArrayList<String> names = new ArrayList<>();
+    private ArrayList<ArrayList<Integer>> matriz = new ArrayList<>();
+    private ArrayList<Integer> linha1 = new ArrayList<>();
+    private ArrayList<Integer> linha2 = new ArrayList<>();
+    private ArrayList<Integer> linha3 = new ArrayList<>();
+
+    //automatizar processo,  para cada linha -> criar um arraaylist com as distancias e adicionar na matriz 
 
     public void execute() {
-        readFirstLine();
-        System.out.println(names);
+        // readFirstLine();
+        // System.out.println(names);
+        linha1.add(1);
+        linha1.add(2);
+        linha1.add(3);
+        linha2.add(4);
+        linha2.add(5);
+        linha2.add(6);
+        linha3.add(7);
+        linha3.add(8);
+        linha3.add(9);
+        matriz.add(linha1);
+        matriz.add(linha2);
+        matriz.add(linha3);
+
+        for (ArrayList<Integer> linha : matriz) {
+            for (int elemento : linha) {
+                System.out.print(elemento + " ");
+            }
+            System.out.println(); 
+        }       
     }
 
-
-
     public void readFirstLine() {
-
         try (Scanner scanner = new Scanner(new File("resources\\DNIT-Distancias.csv"))) {
             scanner.useDelimiter(",");
 
@@ -39,25 +61,4 @@ public class App {
 
 
 
-    public void readAndPrintCSV() {        
-
-
-
-
-
-        try (BufferedReader reader = new BufferedReader(new FileReader("DNIT-Distancias.csv"))) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                // Use StringTokenizer ou split() para dividir a linha em campos
-                String[] fields = line.split(",");
-                // Imprima os campos
-                for (String field : fields) {
-                    System.out.print(field + " ");
-                }
-                System.out.println(); // Nova linha para a próxima linha do CSV
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
 }
